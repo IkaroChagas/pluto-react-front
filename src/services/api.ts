c73bbe71d820b2b887af3a2f915c736f1c87d240
Ikaro <ikarochagas.ig@gmail.com>
2023-08-28 12:28:27 -0300

    api URL updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://pluto-api-7hjw.onrender.com/api"
+    baseURL: "https://console.neon.tech/api/v2/projects/super-frost-68492549/branches/br-still-morning-16182512/api"
 })
 
 export default api
+
+
+
