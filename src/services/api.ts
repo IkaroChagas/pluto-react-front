b4680420199615277de9e67546b56d9658145810
Ikaro <ikarochagas.ig@gmail.com>
2023-08-28 13:14:00 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://console.neon.tech/api/v2/projects/super-frost-68492549/branches/br-still-morning-16182512/api"
+    baseURL: "https://console.neon.tech/api/v2/projects/super-frost-68492549/operations/br-still-morning-16182512/api",
 })
 
 export default api
